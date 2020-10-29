376d43db23a45f12762c3a8aa08d7844aa73c552
Evan Reid <etreid87@gmail.com>
2020-10-28 21:46:43 -0400

    html requests

--- server.js
+++ server.js
@@ -21,3 +21,35 @@ app.get("/api/notes", function(err, res){
     res.json(noteData)
 });
 
+app.post("/api/notes", function(err, res){
+//     // try {
+//         noteData = fs.readFileSync("db/db.json","utf8");
+//         noteData = JSON.parse(noteData);
+//     } catch (err) {
+//         console.log("error");
+//     }
+//     res.json(noteData)
+});
+
+app.delete("/api/notes", function(err, res){
+
+
+
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+  
+ 
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.json(__dirname, "db/db.json"));
+  });
+
+  app.listen(PORT, function() {
+      console.log("The server is listening:" + PORT);
+  });
